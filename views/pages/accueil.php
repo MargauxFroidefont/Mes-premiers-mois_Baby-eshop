a8e909a5a6adfe290a3637dae8ce8114976b4d77
MargauxFroidefont <margaux.froidefont@gmail.com>
2017-05-17 16:44:11 +0200

    :sparkles: Add homepage

--- views/pages/accueil.php
+++ views/pages/accueil.php
@@ -25,20 +25,132 @@
 
   </div>
 
-  <div class="accueil-concept">
+  <div class="accueil-concept accueil-part">
 
-    <div class="accueil-content-left">
-      <img src="src/images/poussin_boite.png" alt="" class="accueil-images">
+    <div class="accueil-content-left content-image">
+      <img src="src/images/poussin_boite.png" alt="" class="accueil-images-right">
     </div>
 
-    <div class="accueil-content-right">
+    <div class="accueil-content-right content-text">
       <p class="satisfy-big-title-bluelight accueil-title">Notre concept</p>
-      <p class="lato-text-bluedark accueil-text">Mes premiers mois, c’est la box mensuelle qui accompa-gnera bébé dans son quotidien grâce à des produits bio et made in France. </p>
+      <p class="lato-text-bluedark accueil-text">Mes premiers mois, c’est la box mensuelle qui accompagnera bébé dans son quotidien grâce à des produits bio et made in France. </p>
       <p class="lato-text-bluedark accueil-text">Chaque mois, recevez dans votre boîte-aux-lettres une boite cadeau contenant six produits adaptés à ses besoins. De l’alimentation à l’hygiène en passant par la puériculture, les jouets et les vêtements, découvrez une déclinaison de produits selon le thème du mois, sélectionnés pour leur originalité et leur qualité. </p>
-      <a href="#" class="button">découvrir</a>
+      <a href="#" class="button accueil-content-button">découvrir</a>
     </div>
 
     <div class="clear-both"></div>
+
+  </div>
+
+  <div class="accueil-advantages accueil-part">
+
+    <div class="accueil-content-left content-text">
+      <p class="satisfy-big-title-orange accueil-title">Pourquoi c'est génial ?</p>
+      <p class="lato-text-bluedark accueil-text">Si vous voulez le meilleur pour votre enfant, vous n’avez pas forcément le temps de surfer sur le web pour déni-cher de nouveaux produits à petit prix. Heureusement, Mes premiers mois s’en charge pour vous !</p>
+      <p class="lato-text-bluedark accueil-text">Ce coffret est une sélection unique, qui vous permet de réaliser 50% d’économies par rapport au prix réel des produits et de faire de nombreuses découvertes chaque mois. Convaincu par un produit ? Retrouvez le sur l’eshop à prix mini ! </p>
+      <a href="#" class="button accueil-content-button">découvrir</a>
+    </div>
+
+    <div class="accueil-content-right content-image">
+      <img src="src/images/poussin_coquille.png" alt="" class="accueil-images-left">
+    </div>
+
+    <div class="clear-both"></div>
+
+  </div>
+
+  <div class="accueil-how-it-works accueil-part">
+
+    <p class="satisfy-big-title-bluedark accueil-title">Comment ça marche ?</p>
+
+    <div class="accueil-how-it-works-steps">
+
+      <div class="accueil-how-it-works-step">
+        <div class="accueil-how-it-works-step-number satisfy-little-title-bluedark">1</div>
+        <p class="lato-footer-light">Choisissez votre formule : une box unique ou un de nos abonnement mensuel </p>
+      </div>
+
+      <div class="accueil-how-it-works-step">
+        <div class="accueil-how-it-works-step-number satisfy-little-title-bluedark">2</div>
+        <p class="lato-footer-light">Personnalisez et commandez votre box facilement et à tout moment depuis chez vous</p>
+      </div>
+
+      <div class="accueil-how-it-works-step">
+        <div class="accueil-how-it-works-step-number satisfy-little-title-bluedark">3</div>
+        <p class="lato-footer-light">Recevez-là chaque première semaine du mois à l'adresse de votre choix</p>
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="accueil-bio accueil-part">
+
+    <div class="accueil-content-left content-image">
+      <img src="src/images/poussin_bio.png" alt="" class="accueil-images-right">
+    </div>
+
+    <div class="accueil-content-right content-text">
+      <p class="satisfy-big-title-bluelight accueil-title">C'est 100% naturel</p>
+      <p class="lato-text-bluedark accueil-text">Comme vous, nous voulons ce qu’il y a de mieux pour vos enfants. Nous sélectionnons donc rigoureusement nos produits pour composer des box qui respectent leur santé. </p>
+      <p class="lato-text-bluedark accueil-text">C’est dans leurs premiers mois de vie que les enfants sont les plus fragiles. Chez nous, c’est tolérance zéro face aux pesticides et autres perturbateurs endocriniens : dans nos box 100% naturelles, les produits sont certifiés biologiques.</p>
+      <a href="#" class="button accueil-content-button">découvrir</a>
+    </div>
+
+    <div class="clear-both"></div>
+
+  </div>
+
+  <div class="accueil-french accueil-part">
+
+    <div class="accueil-content-left content-text">
+      <p class="satisfy-big-title-orange accueil-title">Et c'est français !</p>
+      <p class="lato-text-bluedark accueil-text">Avec nos box engagées, nous sommes soucieux de vous proposer des produits à la production durable, respectueuse de l’humain et de la nature et qui soutient les producteurs locaux et les entreprises françaises.</p>
+      <p class="lato-text-bluedark accueil-text">En cadeau de naissance ou en tant qu’abonnement familial, la box Mes premiers mois est un véritable engagement pour la production durable : même son emballage est biodégradable ! Plus qu'un cadeau, vous offrez à bébé un futur désirable. </p>
+      <a href="#" class="button accueil-content-button">découvrir</a>
+    </div>
+
+    <div class="accueil-content-right content-image">
+      <img src="src/images/poussin_francais.png" alt="" class="accueil-images-left">
+    </div>
+
+    <div class="clear-both"></div>
+
+  </div>
+
+  <div class="accueil-partners accueil-part">
+
+    <p class="satisfy-big-title-bluedark accueil-title">Des partenaires natures et engagés</p>
+    <p class="lato-text-bluedark">Pour vous proposer des produits uniques, nous avons sillonné la France à la rencontre de producteurs et d’artisans passionnés. Nos partenaires sont choisis pour leur sérieux et la qualité de leurs produits est strictement contrôlée et adaptée aux besoins de bébé. </p>
+    <p class="lato-text-bluedark">Rien n’est laissé au hasard pour vous permettre de développer une relation de confiance avec ceux qui nous permettent de prendre naturellement soin de bébé est primordial.</p>
+
+    <div class="accueil-partners-logo">
+
+      <div class="accueil-how-it-works-step">
+        <img src="src/images/partenaire_chabadabada.png" alt="">
+      </div>
+
+      <div class="accueil-how-it-works-step">
+        <img src="src/images/partenaire_alphanova.jpg" alt="">
+      </div>
+
+      <div class="accueil-how-it-works-step">
+        <img src="src/images/partenaire_hevea.jpg" alt="">
+      </div>
+
+      <div class="accueil-how-it-works-step">
+        <img src="src/images/partenaire_klorane.png" alt="">
+      </div>
+
+      <div class="accueil-how-it-works-step">
+        <img src="src/images/partenaire_cattier.jpg" alt="">
+      </div>
+
+      <div class="accueil-how-it-works-step">
+        <img src="src/images/partenaire_babilles.png" alt="">
+      </div>
+
+    </div>
 
   </div>
 

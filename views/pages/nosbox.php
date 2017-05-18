482bd07ce691a0f3cc3cc4c17bb1c192dbf2cb21
MargauxFroidefont <margaux.froidefont@gmail.com>
2017-05-18 21:13:50 +0200

    :sparkles: Add Nos Box page

--- views/pages/nosbox.php
+++ views/pages/nosbox.php
@@ -15,3 +15,160 @@
   </nav>
 
 </section>
+
+<section class="content">
+
+  <div class="nosbox-part">
+
+    <div class="nosbox-content-left">
+      <p class="satisfy-big-title-bluelight nosbox-title">La box de Juin</p>
+      <nav>
+        <ul class="lato-text-bluedark">
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Spray solaire bébé Très Haute Protection ALPHANOVA</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Animaux marins de bain PLAN TOYS</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Maillot de bain anti-UV KATVIG </span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Bouillie à base de céréales BABY PLUS</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Un plat préparé salé HIPP BIOLOGIQUE</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Couche lavable HAMAC</span></li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="nosbox-content-right">
+      <p class="satisfy-little-title-orange">Vacances à la mer</p>
+      <img src="src/images/box_juin.png" alt="" class="nosbox-images">
+      <a href="#" class="button nosbox-button button-buy-box">Commander une box</a>
+      <a href="#" class="button nosbox-button button-buy-products">Voir les produits</a>
+    </div>
+
+    <div class="nosbox-comments-left">
+
+        <h3 class="satisfy-little-title-pink">L'avis des parents</h3>
+        <p class="lato-comment-bluedark">« On a hâte que l'été arrive pour partir au bord de la mer pour tester toutes ses découvertes ! » </p>
+        <p class="lato-comment-bluedark">Parents.fr</p>
+
+    </div>
+
+    <div class="nosbox-comments-right">
+
+        <p class="lato-comment-bluedark">« La couche lavable me fait un peu peur, mais je trouve ça super de proposer des produits originaux et écologiques. Dans tous les cas, je suis conquise par le spray solaire et le maillot anti UV, testé et approuvé le WE dernier ! » </p>
+        <p class="lato-comment-bluedark">Léa, 29 ans</p>
+
+    </div>
+
+    <div class="clear-both"></div>
+
+  </div>
+
+  <div class="nosbox-part">
+
+    <div class="nosbox-content-left">
+      <p class="satisfy-big-title-bluelight nosbox-title">La box de Mai</p>
+      <nav>
+        <ul class="lato-text-bluedark">
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>400g de lait bio 1er ou 2ème âge HOLLE</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Un plat préparé salé HIPP BIOLOGIQUE</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Un baume au Calendula ESSENTIALCARE</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Doudou Lapinou en coton biologique CHABADABADA</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Un body léger rayé CHABADABADA</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Un biberon 800ml NATURAL TOUCH</span></li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="nosbox-content-right">
+      <p class="satisfy-little-title-orange">Une journée au zoo</p>
+      <img src="src/images/box_mai.png" alt="" class="nosbox-images">
+      <a href="#" class="button nosbox-button button-buy-box">Commander une box</a>
+      <a href="#" class="button nosbox-button button-buy-products">Voir les produits</a>
+    </div>
+
+    <div class="nosbox-comments-left">
+
+        <h3 class="satisfy-little-title-pink">L'avis des parents</h3>
+        <p class="lato-comment-bluedark">« Une box totalement dans son thème, qui propose des produits nomades et pratiques à l’effigie des animaux, et toujours bio et made in France ! » </p>
+        <p class="lato-comment-bluedark">Family.fr</p>
+
+    </div>
+
+    <div class="nosbox-comments-right">
+
+        <p class="lato-comment-bluedark">« Un coup de coeur pour le hochet : bébé adore et moi je suis confiante car je connais la qualité des produits avec lesquels il joue. »</p>
+        <p class="lato-comment-bluedark">Emilie, 35 ans</p>
+
+    </div>
+
+    <div class="clear-both"></div>
+
+  </div>
+
+  <div class="nosbox-part">
+
+    <div class="nosbox-content-left">
+      <p class="satisfy-big-title-bluelight nosbox-title">La box de Avril</p>
+      <nav>
+        <ul class="lato-text-bluedark">
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Spray solaire bébé Très Haute Protection ALPHANOVA</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Animaux marins de bain PLAN TOYS</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Maillot de bain anti-UV KATVIG </span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Bouillie à base de céréales BABY PLUS</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Un plat préparé salé HIPP BIOLOGIQUE</span></li>
+          <li class="nosbox-product"><img src="src/images/poussin_list.png" alt="" class="nosbox-list"><span>Couche lavable HAMAC</span></li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="nosbox-content-right">
+      <p class="satisfy-little-title-orange">Le retour du printemps</p>
+      <img src="src/images/box_avril.png" alt="" class="nosbox-images">
+      <a href="#" class="button nosbox-button button-buy-box">Commander une box</a>
+      <a href="#" class="button nosbox-button button-buy-products">Voir les produits</a>
+    </div>
+
+    <div class="nosbox-comments-left">
+
+        <h3 class="satisfy-little-title-pink">L'avis des parents</h3>
+        <p class="lato-comment-bluedark">« Des produits frais et léger, à l’image du printemps, qui favorisent l’autonomie de bébé et font craquer les parents » </p>
+        <p class="lato-comment-bluedark">MarieClaire.fr</p>
+
+    </div>
+
+    <div class="nosbox-comments-right">
+
+        <p class="lato-comment-bluedark">« J’ai adoré cette première box qui contient de véritables indispensables pour ma vie de maman et qui me permet de découvrir de nouvelles marques bio »</p>
+        <p class="lato-comment-bluedark">Linda, 31 ans</p>
+
+    </div>
+
+    <div class="clear-both"></div>
+
+  </div>
+
+  <div class="reinsurance-content">
+
+    <div class="reinsurance-elements">
+      <img src="src/icons/bio.jpg" alt="">
+      <h4 class="lato-reinsurance">Agriculture Biologique</h4>
+      <div class="reinsurance-filet"></div>
+    </div>
+
+    <div class="reinsurance-elements">
+      <img src="src/icons/france.svg" alt="">
+      <h4 class="lato-reinsurance">Made in France</h4>
+      <div class="reinsurance-filet"></div>
+    </div>
+
+    <div class="reinsurance-elements">
+      <img src="src/icons/cadenas.svg" alt="">
+      <h4 class="lato-reinsurance">Paiement privilégié</h4>
+      <div class="reinsurance-filet"></div>
+    </div>
+
+    <div class="reinsurance-elements">
+      <img src="src/icons/livraison.svg" alt="">
+      <h4 class="lato-reinsurance">Livraison Colissimo</h4>
+    </div>
+
+  </div>
+
+</section>

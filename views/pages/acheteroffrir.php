c9701abf3958cf16ba7d073b42f0847977999ffd
MargauxFroidefont <margaux.froidefont@gmail.com>
2017-05-19 22:03:42 +0200

    :sparkles: Add buy and offer page + :lipstick: Responsive

--- views/pages/acheteroffrir.php
+++ views/pages/acheteroffrir.php
@@ -15,3 +15,133 @@
   </nav>
 
 </section>
+
+<section class="content">
+
+  <div class="onebox-part">
+
+    <h1 class="satisfy-little-title-orange">Craquez pour la première box bébé bio et made in France à petit prix !</h1>
+
+    <div class="onebox-left">
+
+      <img src="src/images/box_juin.png" alt="">
+
+    </div>
+
+    <div class="onebox-right">
+
+      <h3 class="satisfy-medium-title-bluelight">Le coffret découverte</h3>
+      <p class="lato-text-bluedark">Recevez ou offrez une box le mois prochain</p>
+      <p class="lato-big-prices">30 €</p>
+
+      <div class="button-content">
+        <a href="#" class="button onebox-offrir-button">Offrir</a>
+        <a href="#" class="button onebox-commander-button">Commander</a>
+      </div>
+
+    </div>
+
+    <div class="clear-both"></div>
+
+  </div>
+
+  <div class="subscription-part">
+
+    <h2 class="satisfy-little-title-orange">Du coffret découverte à l’abonnement, il y a forcément une offre pour vous ou qui comblera vos proches.</h2>
+
+    <div class="subscription-cards">
+
+      <div class="subscription-card">
+
+        <h5 class="satisfy-medium-title-bluelight">3 mois</h5>
+        <p class="lato-text-bluedark">Recevez ou offrez 3 box à partir du mois prochain </p>
+        <p class="lato-big-prices">84 €</p>
+        <p class="lato-more-bluedark">(Soit 28€ par mois)</p>
+
+        <div class="button-content">
+          <a href="#" class="button subscription-offrir-button">Offrir</a>
+          <a href="#" class="button subscription-commander-button">Commander</a>
+        </div>
+
+      </div>
+
+      <div class="subscription-card">
+
+        <h5 class="satisfy-medium-title-bluelight">3 mois</h5>
+        <p class="lato-text-bluedark">Recevez ou offrez 3 box à partir du mois prochain </p>
+        <p class="lato-big-prices">84 €</p>
+        <p class="lato-more-bluedark">(Soit 28€ par mois)</p>
+
+        <div class="button-content">
+          <a href="#" class="button subscription-offrir-button">Offrir</a>
+          <a href="#" class="button subscription-commander-button">Commander</a>
+        </div>
+
+      </div>
+
+      <div class="subscription-card">
+
+        <h5 class="satisfy-medium-title-bluelight">3 mois</h5>
+        <p class="lato-text-bluedark">Recevez ou offrez 3 box à partir du mois prochain </p>
+        <p class="lato-big-prices">84 €</p>
+        <p class="lato-more-bluedark">(Soit 28€ par mois)</p>
+
+        <div class="button-content">
+          <a href="#" class="button subscription-offrir-button">Offrir</a>
+          <a href="#" class="button subscription-commander-button">Commander</a>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="testimony-part">
+
+    <h4 class="satisfy-big-title-bluelight">Qu'en pensent les parents ?</h1>
+
+    <div class="testimony-left">
+      <img src="src/images/temoignage_maman.jpg" alt="">
+    </div>
+
+    <div class="testimony-right">
+      <p class="lato-text-bluedark">« J’ai adoré cette première box qui contient de véritables indispensables pour ma vie de maman et qui me permet de découvrir de nouvelles marques bio » </p>
+    </div>
+
+    <div class="button-content">
+      <a href="#" class="button testimony-button">Lire les témoignages</a>
+    </div>
+
+    <div class="clear-both"></div>
+
+  </div>
+
+  <div class="reinsurance-content">
+
+    <div class="reinsurance-elements">
+      <img src="src/icons/bio.jpg" alt="">
+      <h4 class="lato-reinsurance">Agriculture Bio</h4>
+      <div class="reinsurance-filet"></div>
+    </div>
+
+    <div class="reinsurance-elements">
+      <img src="src/icons/france.svg" alt="">
+      <h4 class="lato-reinsurance">Made in France</h4>
+      <div class="reinsurance-filet"></div>
+    </div>
+
+    <div class="reinsurance-elements">
+      <img src="src/icons/cadenas.svg" alt="">
+      <h4 class="lato-reinsurance">Paiement privilégié</h4>
+      <div class="reinsurance-filet"></div>
+    </div>
+
+    <div class="reinsurance-elements">
+      <img src="src/icons/livraison.svg" alt="">
+      <h4 class="lato-reinsurance">Livraison Colissimo</h4>
+    </div>
+
+  </div>
+
+</section>

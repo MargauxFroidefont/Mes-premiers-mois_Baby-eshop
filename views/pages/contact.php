f266aa5207ada0a893806a98a149ac742c855235
MargauxFroidefont <margaux.froidefont@gmail.com>
2017-05-21 23:16:19 +0200

    :sparkles: Add Contacts page + :lipstick: Responsive Contacts page

--- views/pages/contact.php
+++ views/pages/contact.php
@@ -1,1 +1,64 @@
-<h1> contact </h1>
+<section class="navbar">
+
+  <div class="navbar-logo">
+    <a href="#"><img src="src/images/logo_grand_format.png" alt="Logo Mes premiers mois"></a>
+  </div>
+
+  <nav>
+    <ul>
+      <li class="lato-menu"><a href="#">Accueil</a></li>
+      <li class="lato-menu"><a href="#">Acheter-Offrir</a></li>
+      <li class="lato-menu"><a href="#">Nos box</a></li>
+      <li class="lato-menu"><a href="#">Boutique</a></li>
+      <li class="lato-menu"><a href="#">Le blog</a></li>
+    </ul>
+  </nav>
+
+</section>
+
+<section class="content">
+
+  <div class="contact-title">
+    <p class="satisfy-medium-title-bluelight">Mes Premiers mois</p>
+  </div>
+
+  <div class="contact-paragraph">
+    <p class="satisfy-little-title-orange">Qui sommes nous ?</p>
+    <p class="lato-text-bluedark">Mes premiers mois, c’est une jeune start-up créée par six étudiants en marketing digital. Passionnés de numérique, nous voyons notre monde évoluer, les initiatives solidaires émerger et nous avons eu envie d’y prendre part. Nous voulons léguer un monde meilleur aux générations futures, et nous pensons que les parents et futurs parents sont nos premiers alliés dans cette lutte !</p>
+    <p class="lato-text-bluedark">Notre but ? Rendre accessible et ludique le bio et le made in France grâce à un système de box. Ainsi est né Mes premiers mois, grâce à volonté de construire une communauté autour des valeurs de l’agriculture biologique et française dans la vie du bébé, et ce grâce à ce formidable outil qu’est internet. </p>
+  </div>
+
+  <div class="contact-paragraph">
+    <p class="satisfy-little-title-orange">Contactez-nous !</p>
+    <p class="lato-text-bluedark">Mes premiers mois
+    <br/> 12 rue du château
+    <br/> 75004 Paris
+    <br/> 01.23.45.67.89
+    <br/> contact@mespremiersmois.fr
+    </p>
+  </div>
+
+  <div class="contact-paragraph">
+    <p class="satisfy-little-title-orange">Envie de garder contact ?</p>
+    <p class="lato-text-bluedark">Abonnez-vous à la newsletter pour recevoir en avant première notre veille sur le naturel, nos articles, les thèmes de nos box et des offres uniques ! </p>
+
+    <form class=" form lato-text-bluedark" action="#" method="post">
+      <div class="form-zone">
+          <label class="form-label" for="name">Nom</label>
+          <div class="form-input">
+            <input class="lato-text-grey form-input-size" type="name" name="name" id="name" placeholder="nom">
+          </div>
+        </div>
+        <div class="form-zone">
+          <label class="form-label" for="birth_date">Date de naissance</label>
+          <div class="form-input">
+            <input class="lato-text-grey form-input-size" type="birth_date" name="birth_date" id="birth_date" placeholder="JJ/MM/AA">
+          </div>
+        </div>
+        <div class="button form-button">
+          <input type="submit">
+        </div>
+    </form>
+  </div>
+
+</section>

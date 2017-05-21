3d3cfb8a9c476d4bb848376ed45c9fe02a25879f
MargauxFroidefont <margaux.froidefont@gmail.com>
2017-05-21 22:22:47 +0200

    :sparkles: Legal mentions page + :lipstick: Legal mentions responsive

--- views/pages/mentionslegales.php
+++ views/pages/mentionslegales.php
@@ -1,1 +1,65 @@
-<h1> mentions légales </h1>
+<section class="navbar">
+
+  <div class="navbar-logo">
+    <a href="#"><img src="src/images/logo_grand_format.png" alt="Logo Mes premiers mois"></a>
+  </div>
+
+  <nav>
+    <ul>
+      <li class="lato-menu"><a href="#">Accueil</a></li>
+      <li class="lato-menu"><a href="#">Acheter-Offrir</a></li>
+      <li class="lato-menu"><a href="#">Nos box</a></li>
+      <li class="lato-menu"><a href="#">Boutique</a></li>
+      <li class="lato-menu"><a href="#">Le blog</a></li>
+    </ul>
+  </nav>
+
+</section>
+
+<section class="content">
+
+  <div class="cgv-title">
+    <p class="satisfy-medium-title-bluelight">Mentions légale</p>
+  </div>
+
+  <div class="cgv-paragraph">
+    <p class="lato-text-bluedark">Le site www.mes-premiers-mois.fr est la propriété de l’entreprise : Mes premiers mois
+    <br/> Immatriculée au RCS de BREST sous le numéro 123 456 789
+    </p>
+  </div>
+
+  <div class="cgv-paragraph">
+    <p class="lato-text-bluedark">Et domiciliée au : 12 rue du château, 75004 PARIS
+    <br/> Tel : 01.23.45.67.89
+    <br/> E-mail : contact@mespremiersmois.fr
+    <br/> Directrice de publication et rédaction : Margaux Froidefont
+    <br/> Charte Graphique et Web Design : Lucie Chabaud
+    </p>
+  </div>
+
+  <div class="cgv-paragraph">
+    <p class="satisfy-little-title-orange">Hébergement du Site</p>
+    <p class="lato-text-bluedark">Et domiciliée au : 12 rue du château, 75004 PARIS
+    <br/> OVH – http://www.ovh.com
+    <br/> 2 rue Kellermann
+    <br/> BP 80157
+    <br/> 59100 Roubaix
+    </p>
+  </div>
+
+  <div class="cgv-paragraph">
+    <p class="lato-text-bluedark">Mes premiers mois est une marque déposée à l’INPI.
+    <br/> Le site a été enregistrée à la CNIL sous le numéro 123456789.
+    </p>
+  </div>
+
+  <div class="cgv-paragraph">
+    <p class="lato-text-bluedark">Conformément à la loi « Informatique et libertés » N°78-17 du 6 janvier 1978 vous bénéficiez d’un droit d’accès, de rectification et d’opposition que vous pouvez exercer en vous adressant à contact@mespremiersmois.fr ou par courrier à l’adresse suivante : Mes premiers mois – 12 rue du château, 75004 PARIS
+    </p>
+  </div>
+
+  <div class="cgv-paragraph">
+    <p class="lato-text-bluedark">Crédit photos</p>
+  </div>
+
+</section>
